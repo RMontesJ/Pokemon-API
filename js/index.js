fc416f6bfcefa0909f2ebcce21fa91b8bf00fe56
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2024-08-09 16:26:01 +0200

    You can search pokemon
    Now you can search pokemon by their name or if it name contains what you write

--- js/index.js
+++ js/index.js
@@ -1,28 +1,49 @@
+window.addEventListener('DOMContentLoaded', pedirDatos);
+let divRes = document.getElementById("res");
+let searchValue = document.getElementById("searchValue");
+let btnSearch = document.getElementById("btn-search");
+let allPokemonData = []; // Para almacenar todos los datos de Pokémon
 
-    window.addEventListener('DOMContentLoaded', pedirDatos);
-    let divRes = document.getElementById("res");
-    let searchValue = document.getElementById("searchValue");
-    let btnSearch = document.getElementById("btn-search");
-    
-    function mostrarDatos(datosIn){
-        let textoRes = "";
-        for (let i = 0; i < datosIn.results.length; i++) {
-            textoRes += '<div class="tarjeta">';
-            textoRes += "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + (i+1) + ".png'>";
-            textoRes +=  '<p> Nombre :' + datosIn.results[i].name + "</p>";
-            textoRes +=  '<p> Numero Pokedex :' + (i+1) + "</p>";
-             textoRes += '</div>';
-        }
-        res.innerHTML = textoRes;
+// Obtiene los datos de la API
+function pedirDatos() {
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1025")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        // Almacena el índice junto con los datos de los Pokémon
+        allPokemonData = data.results.map((pokemon, index) => ({
+            ...pokemon,
+            index: index + 1 // Guardamos el índice + 1 para los sprites
+        }));
+        construirDatos(allPokemonData); // Construye las tarjetas inicialmente con todos los Pokémon
+    });
+}
+
+// Crea la tarjeta del Pokémon
+function construirDatos(datosIn) {
+    let textoRes = "";
+    for (let i = 0; i < datosIn.length; i++) {
+        textoRes += '<div class="tarjeta">';
+        textoRes += "<img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + datosIn[i].index + ".png'>"; // Usamos el índice correcto
+        textoRes +=  '<p> Nombre: ' + datosIn[i].name + "</p>";
+        textoRes +=  '<p> Número Pokedex: ' + datosIn[i].index + "</p>"; // Usamos el índice correcto
+        textoRes += '</div>';
     }
-    
-    function pedirDatos(){
-        fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=1025")
-        .then (res => res.json())
-        .then (data => {
-            console.log(data);
-            mostrarDatos(data);
-    
-        })
+    imprimirDatos(textoRes);
+}
+
+// Imprime los datos
+function imprimirDatos(data) {
+    divRes.innerHTML = data;
+}
+
+// Agrega un evento al botón de búsqueda
+btnSearch.addEventListener('click', () => {
+    let searchTerm = searchValue.value.toLowerCase().trim();
+    if (searchTerm) {
+        let filteredData = allPokemonData.filter(pokemon => pokemon.name.includes(searchTerm));
+        construirDatos(filteredData);
+    } else {
+        construirDatos(allPokemonData); // Muestra todos si no hay término de búsqueda
     }
-    
+});
